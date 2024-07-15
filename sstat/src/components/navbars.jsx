7c99b39a528f0a2ee0d3736b6e4c1d93aa8cc51c
Abikimoz <i.m.anisimov25@gmail.com>
2024-07-15 23:35:43 +0300

    create navbar

---
+++ sstat/src/components/navbars.jsx
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+function Navbars() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Col xs={6} md={4}>
+          <Image src="/src/logo.png" rounded />
+        </Col>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Лиги</Nav.Link>
+            <Nav.Link href="#link">Команды</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navbars;
