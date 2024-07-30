fe0ad29d434e9f3ff815239d5ed5b0f3cac4566b
Abikimoz <i.m.anisimov25@gmail.com>
2024-07-30 22:53:33 +0300

    removed the rounding at the SearchBar junction

--- sstat/src/components/SearchBar.jsx
+++ sstat/src/components/SearchBar.jsx
@@ -5,16 +5,17 @@ function SearchBar() {
   return (
     <div className="container px-3 mb-4">
       <Form className="d-flex mt-3 justify-content-start">
-        <FormControl
-          type="search"
-          placeholder="Поиск..."
-          className="mr-2 form-control-sm"
-          style={{ width: '10%' }}
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-primary">
+        <div className="input-group" style={{ width: '20%', minWidth: '200px' }}>
+          <FormControl
+            type="search"
+            placeholder="Поиск"
+            className="form-control-sm rounded-start" // Закругляем только слева
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-primary rounded-end"> {/* Закругляем только справа */}
             <i className="fas fa-search"></i>
-        </button>
+          </button>
+        </div>
       </Form>
     </div>
   );
